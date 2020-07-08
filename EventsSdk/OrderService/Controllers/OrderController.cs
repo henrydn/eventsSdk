07ec5dfb8be7e8196e06cc13bbbb5859f2dcc97c
Henry de Nysschen <Henry.de_Nysschen@bmwgroup.com>
2020-07-08 16:13:00 +0200

    Create base project

---
+++ EventsSdk/OrderService/Controllers/OrderController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OrderService.Controllers
+{
+    [ApiController]
+    [Route("/api/orders")]
+    public class OrderController : ControllerBase
+    {
+        [HttpPost]
+        public async Task CreateOrder()
+        {
+            Console.WriteLine("Order Created");
+        }
+    }
+}
